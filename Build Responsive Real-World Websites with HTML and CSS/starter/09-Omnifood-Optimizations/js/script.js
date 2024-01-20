8e48d8709e5f8b1bc69c939eb3011c3924ba298e
Dafy Yasenova <dafy_yasenova@abv.bg>
2024-01-20 11:36:22 +0200

    implementing smooth scrolling

--- Build Responsive Real-World Websites with HTML and CSS/starter/09-Omnifood-Optimizations/js/script.js
+++ Build Responsive Real-World Websites with HTML and CSS/starter/09-Omnifood-Optimizations/js/script.js
@@ -1,28 +1,59 @@
-const yearEl = document.querySelector(".year");
+const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 
-const btnNav = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
+const btnNav = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
 
-btnNav.addEventListener("click", function(){
-  headerEl.classList.toggle("nav-open");
+btnNav.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
 });
 
+// Smooth scrolling animation 
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    // Scroll back to top
+
+    if (href === '#') window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    // Close mobile navigation
+
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open')
+    }
+
+  })
+
+})
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
 
-  const flex = document.createElement("div");
+  const flex = document.createElement('div');
 
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
 
   document.body.appendChild(flex);
 
@@ -30,7 +61,7 @@ function checkFlexGap() {
   flex.parentNode.removeChild(flex);
   console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add('no-flexbox-gap');
 }
 checkFlexGap();
 
